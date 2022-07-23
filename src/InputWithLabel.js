c8048b98ce71a76b1f7d104f35a56b2102772b80
Xeniya Dobrogorskaya <xeniyad@Xeniyas-iMac.local>
2022-07-23 15:09:12 -0400

    Add Auto-Focus to Input

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -9,6 +9,18 @@ const InputWithLabel = ({
   placeholder,
   children,
 }) => {
+  const inputRef = React.useRef();
+  React.useEffect(() => {
+    inputRef.current.focus();
+  });
+  //   This is the most straightforward usage of useEffect
+  //   where we only pass one argument -- a function.
+  //   This function will render on every render -- meaning
+  //   it runs on the first render of the component
+  //   (also called on mount or mounting of the component)
+  //   and on every re-render of the component
+  //   (also called on update or updating of the component).
+
   return (
     <>
       <label htmlFor={id}>{children} </label>
@@ -19,6 +31,7 @@ const InputWithLabel = ({
         onChange={onChange}
         name={name}
         placeholder={placeholder}
+        ref={inputRef}
       ></input>
     </>
   );
